cd6ad837aa8c77ca1bfae15b8274712465ff4679
ronalt augusto <ronaltaugustocs@gmmail.com>
2023-01-23 01:55:30 -0300

    connected server with web

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -2,14 +2,13 @@ interface ProgressBarProps {
    progress: number
 }
 
-
 export function ProgressBar(props: ProgressBarProps) {
    return (
       <div className="h-3 rounded-xl mt-4 w-full bg-zinc-700">
          <div
             role="progressbar"
             aria-label="Progressos de hábitos completados nesse dia"
-            aria-aria-valuenow={props.progress}
+            aria-valuenow={props.progress}
             className="h-3 rounded-xl  bg-violet-600"
             style={{
                width: `${props.progress}%`
